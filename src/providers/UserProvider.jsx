c4100505fd64b4f3b6feb3b8e5f839ac9c3a3712
keiseke <81750996+keiseke@users.noreply.github.com>
2021-06-21 22:34:14 +0900

    20210621

---
+++ src/providers/UserProvider.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext({});
+
+export const UserProvider = (props) => {
+  const { children } = props;
+  const [userInfo, setUserInfo] = useState(null);
+  const contextName = "みや";
+  return (
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
